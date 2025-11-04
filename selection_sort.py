2d6ff3f0abd7e8fb8b5293dd3be03c9de2c413d2
letnikova1323 <m.letnikova06@gmail.com>
2025-11-04 20:44:49 +0300

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,33 @@
+# 1.
+def selection_sort_asc(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+# 2.
+def selection_sort_desc(arr):
+    n = len(arr)
+    for i in range(n):
+        max_idx = i
+        for j in range(i+1, n):
+            if arr[j] > arr[max_idx]:
+                max_idx = j
+        arr[i], arr[max_idx] = arr[max_idx], arr[i]
+
+# 3. 
+def selection_sort_phones(phones):
+    n = len(phones)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            # Сравниваем числовые значения без дефисов
+            if int(phones[j].replace('-', '')) < int(phones[min_idx].replace('-', '')):
+                min_idx = j
+        phones[i], phones[min_idx] = phones[min_idx], phones[i]
+
+
+    print("Отсортированный список:", phones)
